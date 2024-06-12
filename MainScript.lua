d68e39377f966f840853fbf89f97e9b02f3b2013
ReversedInsides <136033052+ReversedInsides@users.noreply.github.com>
2024-06-12 04:53:43 +0200

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -17,6 +17,35 @@ local RunService = game:GetService("RunService")
 -- Customizable Version: 19:59:28 UTC PM 13/08/2023
 
 -- This script has been converted to FE by jh0nd0eone
+local FancyChatGUI = Instance.new("BillboardGui")
+FancyChatGUI.Enabled = false;
+FancyChatGUI.ClipsDescendants = true;
+FancyChatGUI.ResetOnSpawn = false;
+FancyChatGUI.MaxDistance = 500;
+FancyChatGUI.Name = "FancyChatGUI";
+FancyChatGUI.ZIndexBehavior = Enum.ZIndexBehavior.Sibling;
+FancyChatGUI.ExtentsOffsetWorldSpace = Vector3.new(0,9,0);
+FancyChatGUI.Active = true;
+FancyChatGUI.Size = UDim2.new(10000,0,5,0);
+FancyChatGUI.Parent = script
+
+local Letter = Instance.new("TextLabel")
+Letter.Visible = false;
+Letter.FontSize = Enum.FontSize.Size14;
+Letter.TextColor3 = Color3.new(1,1,1);
+Letter.BackgroundTransparency = 1;
+Letter.Text = "L";
+Letter.TextWrapped = true;
+Letter.TextStrokeTransparency = 0;
+Letter.AnchorPoint = Vector2.new(0.5,0.5);
+Letter.Font = Enum.Font.SourceSansSemibold;
+Letter.Name = "Letter";
+Letter.Size = UDim2.new(0,30,0,30);
+Letter.TextSize = 14;
+Letter.BackgroundColor3 = Color3.new(1,1,1);
+Letter.TextScaled = true;
+Letter.TextWrap = true;
+Letter.Parent = FancyChatGUI
 
 local RemoteEventFunc = nil
 local Player,Mouse,mouse,UserInputService,ContextActionService = game.Players.jh0nd0eone,nil,nil,nil,nil
